eb0c3d3b6d6e18ff08da20e38f9099794761cbb3
Aryan Kumar <Aryanak9163@gmail.com>
2024-08-13 19:53:24 +0530

    surefire plugin && supplier method

--- src/test/java/com/aryankumarofficial/TestShapes.java
+++ src/test/java/com/aryankumarofficial/TestShapes.java
@@ -18,5 +18,15 @@ class TestShapes {
 	void testComputeSquareArea() {
 		assertEquals(576, shapes.computeSquareArea(24));
 	}
+	
+	@Test
+	void testComputeCircleArea() {
+		assertEquals(78.5, shapes.computeCircleArea(5),"area of circle calculation went wrong!");
+	}
+	
+	@Test
+	void testComputeCircleArea_Supplier() {
+		assertEquals(78.5, shapes.computeCircleArea(5),()->"area of circle is wrongly calculated");
+	}
 
 }
