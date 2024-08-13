2d99dc0bc5ad79c0b9d22c89187ea57c3d23186f
Aryan Kumar <Aryanak9163@gmail.com>
2024-08-13 23:42:14 +0530

    test before and after all && classinstance annotation

--- src/test/java/com/aryankumaroffical/TestBeforeAfter.java
+++ src/test/java/com/aryankumaroffical/TestBeforeAfter.java
@@ -2,11 +2,29 @@ package com.aryankumaroffical;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TestBeforeAfter {
+
+	TestBeforeAfter() {
+		System.out.println("Test obj is created before the test method is invocked!");
+	}
+
+	@BeforeAll
+	void beforeAll() {
+		System.out.println("Before all tests");
+	}
+
+	@AfterAll
+	void afterAll() {
+		System.out.println("after all tests");
+	}
 
 	Shapes shapes;
 
@@ -26,9 +44,9 @@ class TestBeforeAfter {
 	void testComputeCircleArea() {
 		assertEquals(78.5, shapes.computeCircleArea(5));
 		System.out.println("actual test");
-		
+
 	}
-	
+
 	@AfterEach
 	void destroy() {
 		System.out.println("after test");
